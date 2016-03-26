4510e1dd0def741d7e6cc25de50104faef632a03
Laurent <laurent.mirabito@cern.ch>
2016-03-26 08:43:24 +0100

    Exampleof structure message to a command

--- test/dummyServer.cxx
+++ test/dummyServer.cxx
@@ -9,8 +9,10 @@ dummyServer::dummyServer(std::string name)
   _fsm->addState("CONFIGURED");
   _fsm->addState("RUNNING");
   _fsm->addTransition("CONFIGURE","CREATED","CONFIGURED",boost::bind(&dummyServer::configure, this,_1));
+    _fsm->addTransition("CONFIGURE","CONFIGURED","CONFIGURED",boost::bind(&dummyServer::configure, this,_1));
   _fsm->addTransition("START","CONFIGURED","RUNNING",boost::bind(&dummyServer::start, this,_1));
-  _fsm->addTransition("STOP","RUNNING","CREATED",boost::bind(&dummyServer::stop, this,_1));
+  _fsm->addTransition("STOP","RUNNING","CONFIGURED",boost::bind(&dummyServer::stop, this,_1));
+  _fsm->addTransition("HALT","RUNNING","CREATED",boost::bind(&dummyServer::stop, this,_1));
 
   //Start server
   std::stringstream s0;
@@ -23,9 +25,23 @@ dummyServer::dummyServer(std::string name)
 void dummyServer::configure(levbdim::fsmmessage* m)
 {
   std::cout<<"Received "<<m->command()<<std::endl;
+  // Delet existing datasources
+  for (std::vector<levbdim::datasource*>::iterator it=_sources.begin();it!=_sources.end();it++)
+      delete (*it);
   _sources.clear();
-   levbdim::datasource* ds= new levbdim::datasource(1,100,0x20000);
-  _sources.push_back(ds);
+  // Add a data source
+  Json::Value jc=m->content();
+  int32_t det=jc["detid"].asInt();
+  const Json::Value& books = jc["sourceid"];
+  for (Json::ValueConstIterator it = books.begin(); it != books.end(); ++it)
+    {
+      const Json::Value& book = *it;
+      int32_t sid=(*it).asInt();
+    // rest as before
+      levbdim::datasource* ds= new levbdim::datasource(det,sid,0x20000);
+      _sources.push_back(ds);
+
+    }
 }
 void dummyServer::start(levbdim::fsmmessage* m)
 {
@@ -34,7 +50,10 @@ void dummyServer::start(levbdim::fsmmessage* m)
 }
 void dummyServer::stop(levbdim::fsmmessage* m)
 {
+  
     std::cout<<"Received "<<m->command()<<std::endl;
+
+    //stop data sources
     for (std::vector<levbdim::datasource*>::iterator it=_sources.begin();it!=_sources.end();it++)
       delete (*it);
     _sources.clear();
